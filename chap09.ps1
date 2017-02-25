c98fe21d3262fd03719315360ab97dc18f39cf44
Jason <livewirerc@gmail.com>
2017-02-25 10:27:51 -0600

    New Files from Chapters 9/10

--- chap09.ps1
+++ chap09.ps1
@@ -1,1 +1,11 @@
-﻿
+get-wmiobject -class Win32_BIOS -computername (get-content .\files\computers.txt)
+
+
+man get-process -ShowWindow
+man import-csv -ShowWindow
+import-csv .\files\computers.csv | gm
+
+get-process -computername (import-csv .\files\computers.csv | select-object -ExpandProperty Hostname)
+
+import-csv .\files\computers.csv | select-object -ExpandProperty Hostname | gm
+ 
