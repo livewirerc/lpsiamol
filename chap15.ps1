c0783f45c886562a1d13ed10145e8085711d1dc2
Jason <livewirerc@gmail.com>
2017-03-04 09:50:54 -0600

    Chap 15 done.

---
+++ chap15.ps1
@@ -0,0 +1,27 @@
+﻿start-job -ScriptBlock {dir}
+start-job -scriptblock {get-eventlog -security -newest 10 -computername localhost}
+get-job
+
+
+enable-psremoting -SkipNetworkProfileCheck
+get-wmiobject win32_operatingsystem -ComputerName localhost -asjob
+
+help * -Parameter asjob
+
+invoke-command -ScriptBlock { get-process } -ComputerName localhost -asjob -JobName Ballzzzzz
+
+get-job -id 10 | fl *
+
+get-job -id 10 | Receive-Job
+
+get-job -id 3 | select-object -ExpandProperty ChildJobs
+receive-job -id 4 -keep
+
+remove-job -id 9
+get-job
+get-job | where { -not $_.HasMoreData } | remove-job
+get-job
+
+receive-job -id 11 -keep
+
+
---
+++ lab15_work.ps1
@@ -0,0 +1,18 @@
+﻿# Task 1: Create one-time background job to find all poweshel cripts on c: drive.
+start-job -ScriptBlock {get-childitem -path c:\ -recurse -filter "*.ps1"}
+get-job
+
+# Task 2:
+notepad .\files\comps.txt
+invoke-command -ComputerName (get-content .\files\comps.txt) -scriptblock {get-childitem -path c:\ -recurse -filter "*.ps1"} -asjob
+get-job -id 15 | select -expand childjobs
+
+# Task 3: 
+
+Register-scheduledjob -name GetSysLogWeekday -scriptblock {
+    Get-EventLog -LogName system -newest 25 | Export-Clixml .\files\System_New.xml 
+} -trigger (new-jobtrigger -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -Weekly -at 6am)
+
+# Task 4:
+get-job
+recieve-job -name GetSysLogWeekday -keep
