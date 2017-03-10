48aeb2e7fd7cea6f6a6630634ae4c27484694575
Jason <livewirerc@gmail.com>
2017-03-10 00:10:19 -0600

    TheLAtest!

---
+++ chap16.ps1
@@ -0,0 +1,17 @@
+﻿help enter-pssession -ShowWindow
+
+enter-pssession nuckles
+exit-pssession
+
+enter-pssession w10e01 -credential atxbs.pvt\jabels
+exit-pssession
+
+gwmi Win32_NetworkAdapterConfiguration
+gwmi Win32_NetworkAdapterConfiguration -filter "Description LIKE '%intel%'"
+
+gwmi Win32_NetworkAdapterConfiguration -filter "Description LIKE '%intel%'" | gm
+
+help Set-NetIPAddress -ShowWindow
+help stop-service -ShowWindow
+
+
---
+++ lab16_work.ps1
@@ -0,0 +1,32 @@
+﻿# Task 1: What methof od a ServiceController object will paus service without stopping it completely.
+get-service | gm
+# Answer: pause()
+
+# Task 2: What methof od a process object would terminate a given process?
+get-process | gm
+# Answer: kill()
+
+# Task 3: What method of a WMI Win32_process object would terminate a given process?
+help get-wmiobject -ShowWindow
+get-wmiobject Win32_process | gm
+# Answer: terminate()
+
+# Task 4: Write four commands that coudl be used to terminate all processes named Notepad.
+# Work:
+start-process Notepad
+get-wmiobject win32_process -filter "name LIKE '%notepad%'" | Invoke-WmiMethod -Name Terminate
+
+# The four commands:
+get-process -name notepad | ForEach-Object { $_.kill()}
+stop-process -name notepad
+get-process -name notepad | stop-process
+get-wmiobject win32_process -filter "name LIKE '%notepad%'" | Invoke-WmiMethod -Name Terminate
+
+# Task 5: Take list of names, display them in all uppercase.
+help case
+help export-formatdata
+get-content .\files\comps.txt | gm
+# This one:
+get-content .\files\comps.txt | ForEach-Object { $_.ToUpper()}
+
+
