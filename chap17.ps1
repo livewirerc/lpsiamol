4d671833a8492f03472f9117fb384be13afe1fb7
Jason <livewirerc@gmail.com>
2017-03-11 14:32:55 -0600

    minor 17

---
+++ chap17.ps1
@@ -0,0 +1,31 @@
+﻿$var = "localhost"
+
+$var
+
+$var | gm
+
+
+get-wmiobject Win32_computersystem -ComputerName localhost
+
+get-wmiobject Win32_computersystem -ComputerName $var
+
+$var = 5
+$var | gm
+$var = 5.5
+
+$computername = 'Nurgle'
+$phrase = "`$computername contains $computername"
+$phrase
+
+$phrase = "`$computername`ncontains`n$computername"
+$phrase
+$computers = 'nurgle','localhost'
+$computers
+$computers | gm
+
+$computers.ToUpper()
+$computers
+$computers.count
+
+
+
